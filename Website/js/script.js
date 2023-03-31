27ad8308e846def6410f2166d25276e1725f2d11
Zoey Le <zoeyle3294@gmail.com>
2023-03-31 02:37:48 -0500

    Add QR Scan js

--- Website/js/script.js
+++ Website/js/script.js
@@ -365,32 +365,5 @@ document.onclick = function (e){
         search_box.classList.add("hide");
     }
 }
-function toggleNav() {
-    document.body.classList.toggle("nav-open");
-    document.querySelector(".nav-links-container").classList.toggle("fade-out-anim");
-  }
 
-// const btn = document
-//     .querySelector('.read-more-btn');
 
-// const text = document
-//     .querySelector('.card__read-more');
-
-// const cardHolder = document
-//     .querySelector('.card-holder');
-
-// cardHolder
-//     .addEventListener('click', e => {
-
-//         const current = e.target;
-
-//         const isReadMoreBtn = current.className.includes('read-more-btn');
-
-//         if (!isReadMoreBtn)
-//             return;
-
-//         const currentText = e.target.parentNode.querySelector('.card__read-more');
-
-//         currentText.classList.toggle('card__read-more--open');
-
-//     });
